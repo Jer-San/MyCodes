e1030993466f6080234efc1ffe887b82ef246da2
Jer-San <jeromesanthiyagu68@gmail.com>
2025-04-01 00:16:50 +0530

    Update 219ContainsDuplicateII.java

--- Easy/219ContainsDuplicateII.java
+++ Easy/219ContainsDuplicateII.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int right=0;right<nums.length;right++){
+            int n = nums[right]; 
+            if(map.containsKey(n)){
+                if(Math.abs(map.get(n)-right) <= k){
+                    return true;
+                }
+            }
+            map.put(n, right);
+        }
+        return false;
+    }
+}
